cb0872878839cf855b52c77d1acdbab4c4e8aecd
AbhiAva <aavadhani.aa@gmail.com>
2021-11-18 19:02:19 +0530

    added configuration files

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+# Taking the latest pull
+echo Taking latest pull ....
+git pull
+
+# Restart a supervisor server instance
+# echo Restarting supervisor ....
+# supervisorctl stop all
+# supervisorctl start all
