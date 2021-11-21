40ef1c9695e85d305d6c0ab64c2497c840602799
AbhiAva <aavadhani.aa@gmail.com>
2021-11-21 17:03:20 +0530

    resolved deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@
 echo Taking latest pull ....
 git pull
 
+# Copying env variables
+echo Copying Environment Variables ....
+cp catalyst_count/catalyst_count/.env.local catalyst_count/catalyst_count/.env
+
+# Activating the Environment
+echo Activating the Environment ....
+source django-app/bin/activate
+
 # Restart a supervisor server instance
 # echo Restarting supervisor ....
 # supervisorctl stop all
